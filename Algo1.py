b621ded020b26b57dca032908628aa42e6113bd8
Farine <1farine.julien@gmail.com>
2025-01-13 15:38:40 +0100

    Décompression finis

--- Algo1.py
+++ Algo1.py
@@ -2,13 +2,20 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-row = 1 #max 75
-col = 1 #max 150
+row = 30 #max 75
+col = 50 #max 150
 image = plt.imread("pns_original.png") #1200 * 600 * 3
 M = image[(row-1)*8:row*8, (col-1)*8:col*8, 2] * 256
 M = M.astype('int32')
 M += - 128
-
+Q = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+              [12, 12, 14, 19, 26, 58, 60, 55],
+              [14, 13, 16, 24, 40, 57, 69, 56],
+              [14, 17, 22, 29, 51, 87, 80, 62],
+              [18, 22, 37, 56, 68, 109, 103, 77],
+              [24, 35, 55, 64, 81, 104, 113, 92],
+              [49, 64, 78, 87, 103, 121, 120, 101],
+              [72, 92, 95, 98, 112, 100, 103, 99]])
 def P(n):
     P = np.zeros((n,n))
     for i in range(n):
@@ -22,4 +29,25 @@ def P(n):
 P = P(8)
 PT = np.transpose(P)
 D = np.dot(np.dot(P, M), PT)
-print(D)
+
+for i in range(8):
+    for j in range(8):
+        D[i][j] = int(D[i][j] / Q[i][j])
+        if j == 7:
+            D[i][j] = 0
+        if i == 7:
+            D[i][j] = 0
+
+# Décompression :
+for i in range(8):
+    for j in range(8):
+        D[i][j] = int(D[i][j] * Q[i][j])
+
+M_new = np.dot(np.dot(PT, D), P)
+
+plt.subplot(1, 2, 1) # row 1, col 2 index 1
+plt.imshow(M)
+plt.subplot(1, 2, 2) # index 2
+plt.imshow(M_new)
+plt.show()
+print(M - M_new)
