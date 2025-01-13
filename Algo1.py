d2ab725222cd076b8be94a9c6aae1128621423d2
Farine <1farine.julien@gmail.com>
2025-01-13 12:18:53 +0100

    Update Algo1.py

--- Algo1.py
+++ Algo1.py
@@ -2,11 +2,12 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-
-image = plt.imread("Campus_SophiaTech_Panneau.jpg") #1280 * 1280 * 3
-
-def creation_bloc_8(image): 
-    bloc = []
-    size = len(image)
-    for i in range(size/8):
-
+row = 1 #max 75
+col = 1 #max 150
+image = plt.imread("pns_original.png") #1200 * 600 * 3
+bloc = image[(row-1)*8:row*8, (col-1)*8:col*8, 0] * 256
+bloc = bloc.astype('int32')
+bloc = bloc - 128
+print(bloc)
+plt.imshow(bloc)
+plt.show()
