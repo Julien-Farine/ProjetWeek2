64f6f36a6026b33ee2dd9d9385cf0b0b81059660
Farine <1farine.julien@gmail.com>
2025-01-13 10:54:32 +0100

    added pictures

--- Algo1.py
+++ Algo1.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 image = plt.imread("Campus_SophiaTech_Panneau.jpg") #1280 * 1280 * 3
 
-def creation_bloc_8(image)
+def creation_bloc_8(image): 
     bloc = []
     size = len(image)
     for i in range(size/8):
-        bloc.append()
+
