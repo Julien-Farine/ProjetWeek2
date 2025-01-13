e8e04caa76bb54e12c213fc60656bb90eb2c23d7
Farine <1farine.julien@gmail.com>
2025-01-13 15:42:45 +0100

    Ajout de commentaires

--- Algo1.py
+++ Algo1.py
@@ -1,13 +1,15 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
+#Choix du bloc 8*8
 row = 30 #max 75
 col = 50 #max 150
-image = plt.imread("pns_original.png") #1200 * 600 * 3
-M = image[(row-1)*8:row*8, (col-1)*8:col*8, 2] * 256
-M = M.astype('int32')
-M += - 128
+image = plt.imread("pns_original.png") #Lecture image
+M = image[(row-1)*8:row*8, (col-1)*8:col*8, 2] * 255 # Prendre un bloc 8*8 + passage valeur de 0-1 --> 0-255
+M = M.astype('int32') # Passage de float32 à int32
+M += - 128 # Passage de 0-255 à -128-127
+
+#Création de la matrice de Quantification Q
 Q = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
               [12, 12, 14, 19, 26, 58, 60, 55],
               [14, 13, 16, 24, 40, 57, 69, 56],
@@ -16,26 +18,25 @@ Q = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
               [24, 35, 55, 64, 81, 104, 113, 92],
               [49, 64, 78, 87, 103, 121, 120, 101],
               [72, 92, 95, 98, 112, 100, 103, 99]])
-def P(n):
-    P = np.zeros((n,n))
-    for i in range(n):
-        for j in range(n):
-            P[i][j] = 0.5 * math.cos(((2*j+1)*i*math.pi)/16)
-            if i == 0:
-                P[i][j] = P[i][j] * 2**-0.5
-    return P
+
+#Création de la matrice P
+P = np.zeros((8,8))
+for i in range(8):
+    for j in range(8):
+        P[i][j] = 0.5 * math.cos(((2*j+1)*i*math.pi)/16)
+        if i == 0:
+            P[i][j] = P[i][j] * 2**-0.5
 
 
-P = P(8)
-PT = np.transpose(P)
+PT = np.transpose(P) #P transposé (égal à P**-1)
 D = np.dot(np.dot(P, M), PT)
 
 for i in range(8):
     for j in range(8):
         D[i][j] = int(D[i][j] / Q[i][j])
-        if j == 7:
+        if j == 7: #Dernière colonne = 0
             D[i][j] = 0
-        if i == 7:
+        if i == 7: #Dernière ligne = 0
             D[i][j] = 0
 
 # Décompression :
@@ -44,6 +45,8 @@ for i in range(8):
         D[i][j] = int(D[i][j] * Q[i][j])
 
 M_new = np.dot(np.dot(PT, D), P)
+
+# Test
 
 plt.subplot(1, 2, 1) # row 1, col 2 index 1
 plt.imshow(M)
