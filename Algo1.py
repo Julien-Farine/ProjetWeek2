ad9409fd7fa48d163765e90012343ce7af8d00ad
Farine <1farine.julien@gmail.com>
2025-01-13 10:45:33 +0100

    initialisation projet

--- Algo1.py
+++ Algo1.py
@@ -1,4 +1,12 @@
 import numpy as np
 import math
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
+
+image = plt.imread("Campus_SophiaTech_Panneau.jpg") #1280 * 1280 * 3
+
+def creation_bloc_8(image)
+    bloc = []
+    size = len(image)
+    for i in range(size/8):
+        bloc.append()
